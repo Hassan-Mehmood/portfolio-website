cfccc132f4bb0fcb6e3a0c411db7432921251ece
Hassan Mehmood <hassanafridi1913@gmail.com>
2022-09-15 15:41:55 +0500

    About me section done

---
+++ src/components/About/About-Styled.ts
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  margin-bottom: 100rem;
+`;
+export const Wrapper = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 4rem 2rem 0;
+
+  @media (min-width: 761px) {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
+export const TextSection = styled.div`
+  text-align: center;
+  h2 {
+    font-size: 1.5rem;
+    color: #000a2d;
+    font-family: "Rufina", serif;
+    font-weight: 700;
+    margin-bottom: 2rem;
+
+    @media (min-width: 545px) {
+      font-size: 2rem;
+    }
+  }
+
+  p {
+    color: #10285d;
+    line-height: 1.5;
+    font-size: 1.1rem;
+    font-family: "Rubik", sans-serif;
+    margin-bottom: 1rem;
+  }
+
+  @media (min-width: 761px) {
+    flex: 1;
+    text-align: left;
+  }
+`;
+
+export const SocialIcons = styled.div`
+  .linkedin {
+    color: #0077b5;
+  }
+  .insta {
+    color: #fb3958;
+    margin: 0 1rem;
+  }
+  .facebook {
+    color: #4267b2;
+  }
+`;
+
+export const ImageSection = styled.div`
+  img {
+    display: block;
+    max-width: 100%;
+    margin: 0 auto;
+  }
+  @media (min-width: 761px) {
+    flex: 1;
+  }
+`;
+export const Text = styled.div``;
---
+++ src/components/About/About.tsx
@@ -0,0 +1,62 @@
+import { BsLinkedin } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
+import { AiFillFacebook } from "react-icons/ai";
+import {
+  Section,
+  Wrapper,
+  TextSection,
+  ImageSection,
+  Text,
+  SocialIcons,
+} from "./About-Styled";
+
+const About = () => {
+  return (
+    <Section>
+      <Wrapper>
+        <ImageSection>
+          <img src={require("../../images/about-me.jpg")} alt="" />
+        </ImageSection>
+        <TextSection>
+          <Text>
+            <h2>I make Websites come alive</h2>
+            <p>
+              I'm a MERN Stack developer having vast experience in JavaScript
+              Web Development. I am a Man with with a CAN-DO atitude and I'm not
+              afraid to take challenges.
+            </p>
+            {/* <h2>Make your business shine on the web</h2> */}
+          </Text>
+          <SocialIcons>
+            <a
+              href="https://www.linkedin.com/in/imhassan-mehmood/"
+              target={"_blank"}
+              rel="noreferrer"
+              className="linkedin"
+            >
+              <BsLinkedin size={18} />
+            </a>
+            <a
+              href="https://www.instagram.com/hassan_afridii1/"
+              target={"_blank"}
+              rel="noreferrer"
+              className="insta"
+            >
+              <FiInstagram size={19} />
+            </a>
+            <a
+              href="https://www.facebook.com/Hassanafridia"
+              target={"_blank"}
+              rel="noreferrer"
+              className="facebook"
+            >
+              <AiFillFacebook size={20} />
+            </a>
+          </SocialIcons>
+        </TextSection>
+      </Wrapper>
+    </Section>
+  );
+};
+
+export default About;
